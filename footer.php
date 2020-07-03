60c2b29dd4038dd2d202ce12ec5077da82446f03
sultanramadhan92 <64899934+sultanramadhan92@users.noreply.github.com>
2020-07-03 18:48:25 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>footer</title>
+		<link href="style.css" rel="stylesheet"
+type="text/css" />
+	</head>
+	<body>
+		<div class="container">
+			<footer>
+				<p>&copy;Sultanra 2020, Universitas Pelita Bangsa</p>
+			</footer>
+		</div>
+		</div>
+	</body>
+</html>
